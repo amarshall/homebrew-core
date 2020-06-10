4109818115133eb5c9e61f76b183a6772235130b
Andrew Marshall <andrew@johnandrewmarshall.com>
2020-06-10 09:36:21 -0400

    rust-analyzer: Correct incorrectly-parsed version

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -3,6 +3,7 @@ class RustAnalyzer < Formula
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-analyzer/rust-analyzer/archive/2020-04-20.tar.gz"
   sha256 "da47d24f24205c77bd8aeba4f0c2d6b7b12f2462ceab7e19473282c9946bd69c"
+  version "2020-04-20"
 
   bottle do
     cellar :any_skip_relocation
